58086222e62f1616312173db9c80463af5416a2c
Дмитрий <dimkaiv@list.ru>
2015-07-09 16:27:35 +0300

    This forty second and forty third tasks.

--- ivanchuk/src/task42/service/ExpenseService.java
+++ ivanchuk/src/task42/service/ExpenseService.java
@@ -1,12 +1,13 @@
 package task42.service;
 
+import task42.dao.ExpenseDAO;
 import task42.dao.ExpenseDAOImpl;
 import task42.dto.Expense;
 
 import java.util.List;
 
 public class ExpenseService {
-    ExpenseDAOImpl expenseDAO = new ExpenseDAOImpl();
+    ExpenseDAO expenseDAO = new ExpenseDAOImpl();
 
     public void addExpense(String date, int receiver, double amount) {
         Expense expense = new Expense(date, receiver, amount);
